0717fb135874b840a0fd6630e4a264b555e61c23
Quentin Aubouin <quentin.aubouin@universite-paris-saclay.fr>
2026-06-25 17:55:35 +0200

    fix webapps

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bombertime-mobile-v1';
+const CACHE_NAME = 'bombertime-mobile-v2';
 const urlsToCache = [
   './mobile.html',
   './manifest.json',
@@ -6,15 +6,36 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    fetch(event.request).then(response => {
+      // Mise en cache de la nouvelle version si on a du réseau
+      if (response && response.status === 200 && response.type === 'basic') {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseClone));
+      }
+      return response;
+    }).catch(() => {
+      // Si on n'a plus de réseau, on charge la version en cache
+      return caches.match(event.request);
+    })
   );
 });
